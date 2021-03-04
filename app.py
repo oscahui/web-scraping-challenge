727b3577de8d39eda83ba38587f2d30b3656ecf7
ohui <74703654+oscahui@users.noreply.github.com>
2021-03-04 20:40:01 +1100

    v2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+import mission_to_mars
 import sys
 
 app = Flask(__name__)
@@ -9,13 +9,12 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 @app.route("/")
 def index():
-    mars_data = mongo.db.mars.find_one()
-    # print(mars)
+    mars_data = mongo.db.mars.find_one()   
     return render_template("index.html", data=mars_data)
 
 @app.route("/scrape")
 def scrape():  
-    mars_scrape = scrape_mars.scrape()
+    mars_scrape = mission_to_mars.scrape()
     print(mars_scrape)
     mongo.db.mars.update({}, mars_scrape, upsert=True)
     return redirect("http://localhost:5000/", code=302)
